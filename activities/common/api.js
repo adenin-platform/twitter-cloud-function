6ac6b8cb158b8d4418c7ed771a062fcb87d38541
olliejm <olliejm@protonmail.com>
2019-02-22 16:24:52 +0000

    spacing rules, new generator changes, bug fix

--- activities/common/api.js
+++ activities/common/api.js
@@ -9,62 +9,62 @@ const endpoint = 'https://api.twitter.com/1.1';
 let _activity = null;
 
 function api(path, opts) {
-    if (typeof path !== 'string') {
-        return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
+  if (typeof path !== 'string') {
+    return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
+  }
+
+  opts = Object.assign({
+    json: true,
+    token: _activity.Context.connector.token,
+    endpoint: endpoint,
+    agent: {
+      http: new HttpAgent(),
+      https: new HttpsAgent()
     }
+  }, opts);
 
-    opts = Object.assign({
-        json: true,
-        token: _activity.Context.connector.token,
-        endpoint: endpoint,
-        agent: {
-            http: new HttpAgent(),
-            https: new HttpsAgent()
-        }
-    }, opts);
+  opts.headers = Object.assign({
+    accept: 'application/json',
+    'user-agent': 'adenin Digital Assistant, https://www.adenin.com/digital-assistant/'
+  }, opts.headers);
 
-    opts.headers = Object.assign({
-        accept: 'application/json',
-        'user-agent': 'adenin Digital Assistant, https://www.adenin.com/digital-assistant/'
-    }, opts.headers);
+  if (opts.token) {
+    opts.headers.Authorization = `Bearer ${opts.token}`;
+  }
 
-    if (opts.token) {
-        opts.headers.Authorization = `Bearer ${opts.token}`;
-    }
+  const url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
 
-    const url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
+  if (opts.stream) {
+    return got.stream(url, opts);
+  }
 
-    if (opts.stream) {
-        return got.stream(url, opts);
-    }
-
-    return got(url, opts).catch((err) => {
-        throw err;
-    });
+  return got(url, opts).catch((err) => {
+    throw err;
+  });
 }
 
 const helpers = [
-    'get',
-    'post',
-    'put',
-    'patch',
-    'head',
-    'delete'
+  'get',
+  'post',
+  'put',
+  'patch',
+  'head',
+  'delete'
 ];
 
 api.stream = (url, opts) => got(url, Object.assign({}, opts, {
-    json: false,
-    stream: true
+  json: false,
+  stream: true
 }));
 
 api.initialize = function (activity) {
-    _activity = activity;
+  _activity = activity;
 };
 
 for (const x of helpers) {
-    const method = x.toUpperCase();
-    api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
-    api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
+  const method = x.toUpperCase();
+  api[x] = (url, opts) => api(url, Object.assign({}, opts, { method }));
+  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, { method }));
 }
 
 module.exports = api;
--- activities/common/utils.js
+++ activities/common/utils.js
@@ -3,38 +3,38 @@
 const got = require('got');
 
 module.exports = {
-    async accessToken(activity) {
-        const credentials = Buffer.from(
-            rfcEncode(activity.Context.connector.clientId) +
-            ':' +
-            rfcEncode(activity.Context.connector.custom1)
-        ).toString('base64');
+  async accessToken(activity) {
+    const credentials = Buffer.from(
+      rfcEncode(activity.Context.connector.clientId) +
+      ':' +
+      rfcEncode(activity.Context.connector.custom1)
+    ).toString('base64');
 
-        const opts = {
-            method: 'POST',
-            headers: {
-                Authorization: 'Basic ' + credentials,
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-            },
-            body: 'grant_type=client_credentials'
-        };
+    const opts = {
+      method: 'POST',
+      headers: {
+        Authorization: 'Basic ' + credentials,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      body: 'grant_type=client_credentials'
+    };
 
-        const response = await got('https://api.twitter.com/oauth2/token', opts);
-        const json = JSON.parse(response.body);
+    const response = await got('https://api.twitter.com/oauth2/token', opts);
+    const json = JSON.parse(response.body);
 
-        if (json.token_type === 'bearer') {
-            return json.access_token;
-        }
-
-        return null;
+    if (json.token_type === 'bearer') {
+      return json.access_token;
     }
+
+    return null;
+  }
 };
 
 function rfcEncode(key) {
-    return encodeURIComponent(key)
-        .replace(/!/g, '%21')
-        .replace(/'/g, '%27')
-        .replace(/\(/g, '%28')
-        .replace(/\)/g, '%29')
-        .replace(/\*/g, '%2A');
+  return encodeURIComponent(key)
+    .replace(/!/g, '%21')
+    .replace(/'/g, '%27')
+    .replace(/\(/g, '%28')
+    .replace(/\)/g, '%29')
+    .replace(/\*/g, '%2A');
 }
